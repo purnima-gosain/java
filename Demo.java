074c22f5c71954fe5d21cd732758ee4e4d16f435
purnima-gosain <purnimagosain78@gmail.com>
2023-06-12 13:07:45 +0545

    first commit

---
+++ Demo.java
@@ -0,0 +1,26 @@
+ class Demo{
+
+
+
+public static void main(String a[]) 
+{
+    int num1 = 4;
+    byte by = 125;
+    short sh = 558;
+    long l = 52456l;
+    float f = 5.6f;
+    double d = 5.6;
+    char c = 'k';
+    boolean b = true;
+    String strn = "hello";
+    
+
+
+
+
+    System.out.println("Hello");
+
+
+ } 
+}
+
