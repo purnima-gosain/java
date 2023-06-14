29c80be6f3c2950c66e7754008658a0ca373735e
purnima-gosain <purnimagosain78@gmail.com>
2023-06-14 12:48:19 +0545

    checked the solutions

--- Exercise1.java
+++ Exercise1.java
@@ -2,11 +2,11 @@ public class Exercise1 {
     public static void main(String[] args){
         int x= 1;
         while(x<10){
-
+            x = x+1;
             if(x> 3){
                 System.out.println("big X");
             }
-            x = x+1;
+          
         }
     }
 }
