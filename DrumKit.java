cc031a069f41f470d7f007ec644b270f714078b0
purnima-gosain <purnimagosain78@gmail.com>
2023-06-16 17:27:51 +0545

    added drumkit class

---
+++ DrumKit.java
@@ -0,0 +1,27 @@
+public class DrumKit{
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playTopHat(){
+        System.out.println("ding ding da-dang");
+    }
+
+    void playSnare(){
+        System.out.println("bang bang ba-bang");
+    }
+}
+
+class DrumKitTestDrive{
+    public static void main(String[] args) {
+
+        DrumKit d = new DrumKit();
+        d.snare = false;
+        d.playSnare();
+        d.playTopHat();
+        if(d.snare == true){
+            d.playSnare();
+        }
+     
+        
+    }
+}
