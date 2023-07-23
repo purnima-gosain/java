d7c6a97f5f4e654a62986f71bf1445de48752181
purnima-gosain <purnimagosain78@gmail.com>
2023-07-23 14:26:21 +0545

    chapter practise

---
+++ Triangle.java
@@ -0,0 +1,19 @@
+// public class Triangle {
+//     double area;
+//     int height;
+//     int length;
+
+//     public static void main(String[] args) {
+//         int x = 0;
+       
+//         Triangle[] ta = new Triangle[4];
+
+       
+//         while( x < 4){
+//               int y =x;
+//               .height = (x+1) * 2;
+//         }
+        
+//     }
+    
+// }
