6f125a280b0e1400dbe20df618e63e9502696804
purnima-gosain <purnimagosain78@gmail.com>
2023-07-25 17:47:56 +0545

    added helper and dotcom class

--- DotComBust.java
+++ DotComBust.java
@@ -1,3 +1,13 @@
+import java.util.*;
+
 public class DotComBust {
-    
+    private GameHelper gameHelper = new GameHelper();
+    private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>();
+    private int numOfGuesses = 0;
+
+    private void setUpGame(){
+        DotCom one = new DotCom();
+        one.setName("Pets.com"); 
+    }
+
 }
