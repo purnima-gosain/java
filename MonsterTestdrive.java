80dc2f73202da241c8a62ee9c7da58536ca48a33
purnima-gosain <gosainpurnima916@gmail.com>
2023-08-07 13:16:05 +0545

    added main method in testdrive class

--- MonsterTestdrive.java
+++ MonsterTestdrive.java
@@ -1,5 +1,14 @@
 public class MonsterTestdrive {
-    
+    public static void main(String[] args){
+        Monster [] ma = new Monster[3];
+        ma[0] = new Vampire();
+        ma[1] = new Dragon();
+        ma[2] = new Monster();
+
+        for(int x = 0; x<3; x++){
+            ma[x].frighten(x);
+        }
+    }
 }
 
 class Monster{
